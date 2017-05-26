bd8ffe50da8eb98418dc542fb340d905cc5acb10
Kevin <contact.kevs@gmail.com>
2017-05-26 11:43:07 +0800

    Add script for dev, prepared file structures and Root Component

---
+++ src/js/client.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from './components/Root';
+
+const abb = document.getElementById("app");
+
+ReactDOM.render(<Root/>,app);
