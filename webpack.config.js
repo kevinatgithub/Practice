c23b7ac75bb1f0d97f65445fdef6addc6498186b
Kevin <contact.kevs@gmail.com>
2017-05-26 11:37:29 +0800

    configs webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    context : path.join(__dirname,'src'),
+    devtool : 'inline-sourcemap',
+    entry : './js/client.js',
+    output : {
+        path : __dirname + '/src/',
+        filename : 'client.min.js'
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                exclude : /(node_modules|bower_components)/,
+                loader : 'babel-loader',
+                query : {
+                    presets : ['react','es2015','stage-0'],
+                    plugins : ['react-html-attrs','transform-decorators-legacy','transform-class-properties']
+                }
+            }
+        ]
+    }
+}
